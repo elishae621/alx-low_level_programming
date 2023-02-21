9e610668e5af719942c5475479cb80a77dde0643
Emmanuel Elisha <elishae621@gmail.com>
2023-02-21 14:52:30 -0800

    add betty style to 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Print "Holberton" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
 int main(void)
 {
 	printf("_putchar\n");
