b7951541c2578789d1571439f082bb00df6f6205
Emmanuel Elisha <elishae621@gmail.com>
2023-03-01 05:16:39 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest - destination of concatenation
+ * @src - string to be added to dest
+ *
+ * Return: concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len1 = 0, len2 = 0;
+
+	while (dest[len1] != '\0')
+		len1++;
+
+	while (src[len2] != '\0')
+	{
+		dest[len1++] = src[len2++];
+	}
+	
+	return (dest);
+}
