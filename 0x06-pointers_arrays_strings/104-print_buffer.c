2cf45bea2b7242ce93eca6cf71edee2dc11a04d6
Emmanuel Elisha <elishae621@gmail.com>
2023-03-02 13:00:08 -0800

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer
+ * @size: int size
+ */
+void print_buffer(char *b, int size)
+{
+	if (size <= 0)
+		printf("\n");
+	return (b);
+}
