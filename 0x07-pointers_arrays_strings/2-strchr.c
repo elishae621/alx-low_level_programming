47e1d22e3e2274164a50da501d490816e7a13fc3
Emmanuel Elisha <elishae621@gmail.com>
2023-03-06 03:54:14 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ *
+ * Return: pinter to first occurence of character in string or NULL if none
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] && s[i] != c)
+	{
+		i++;
+	}
+
+	if (s[i] == '\0')
+		return (NULL);
+	else
+		return (s + i);
+}
