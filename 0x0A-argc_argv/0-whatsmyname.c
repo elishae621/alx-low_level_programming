8f8803b99eedcb10be65d37685605ab1d4bba1a1
Emmanuel Elisha <elishae621@gmail.com>
2023-03-10 12:26:18 -0800

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name
+ * @argc: number of args
+ * @argv: pointer to arg array
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
