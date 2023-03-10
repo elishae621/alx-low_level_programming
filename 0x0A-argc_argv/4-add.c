20700ef9edf36e50695925c94ca56c2ae6139035
Emmanuel Elisha <elishae621@gmail.com>
2023-03-10 13:29:37 -0800

    4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - multiplies two numbers
+ * main - adds positive numbers
  * @argc: number of args
  * @argv: pointer to arg array
  * Return: 0 for success and 1 for error
  */
 int main(int argc, char *argv[])
 {
-	if (argc == 3)
+	if (argc > 1)
 	{
-		int i = 1, sum = 1;
-		
+		int i = 1, sum = 0, value;
+
 		while(i < argc)
 		{
-			sum *= atoi(argv[i]);
+			value = atoi(argv[i]);
+			if(isdigit(value) == 0)
+			{
+				sum += value;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
 			i++;
 		}
 		printf("%d\n", sum); 
@@ -23,7 +33,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		printf("Error\n");
-		return (1);
+		printf("0\n");
+		return (0);
 	}
 }
