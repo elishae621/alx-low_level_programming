27429adc83af6fb6ca50b8f3575670dd07200f48
Emmanuel Elisha <elishae621@gmail.com>
2023-03-13 04:57:58 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string
+ * @str: string
+ *
+ * Return: pointer to string or NULL if it fails
+ */
+char *_strdup(char *str)
+{
+	unsigned long int i = 0;
+	char *dup;
+	int size = 0;
+
+	while (str[size++])
+		;
+
+	if (size <= 0)
+		return (NULL);
+
+	dup = malloc(sizeof(char) * size);
+
+	if (dup == NULL)
+		return (NULL);
+
+	while (i < (sizeof(char) * size))
+	{
+		dup[i] = str[i];
+	       i++;
+	}
+
+	return (dup);
+}
