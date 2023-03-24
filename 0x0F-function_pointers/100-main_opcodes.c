5671fa90e858b0e5363dd87f15e722b280634b45
Emmanuel Elisha <elishae621@gmail.com>
2023-03-24 03:00:43 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code 
+ * @argc: amount of args
+ * @argv: argumbers
+ * Return: 1 if an error occured, 0 otherwis
+ */
+int main(int argc, char *argv[])
+{
+	unsigned char *p = (unsigned char *)main;
+
+	unsigned int bytes;
+
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	bytes = atoi(argv[1]);
+
+	for (i = 0; i < bytes; i++)
+		printf("%02x ", *p++);
+	printf("\n");
+	return (0);
+}
