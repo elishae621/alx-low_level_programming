e910619657d3944d8b18bee374b3c1bf224c2bc7
Emmanuel Elisha <elishae621@gmail.com>
2023-04-07 09:24:54 -0700

    use read instead of fgetc

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,8 @@
-#include <stddef.h>
-#include <stdio.h>
+#include "main.h"
+#include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
@@ -10,27 +12,29 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char c;
-	size_t n = 0, printed = 0;
-	FILE *file;
+	int file;
+	ssize_t rd, wd;
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * letters);
 
 	if (filename == NULL)
 		return (0);
 
-	file = fopen(filename, "r");
-
-	if (file == NULL)
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 		return (0);
 
-	while ((c = fgetc(file)) != EOF && n < letters)
-	{
-		printed =  write(1, &c, 1);
-		if (printed != 1)
-			return (0);
-		n += printed;
-	}
+	rd = read(file, buffer, letters);
+	if (rd == -1)
+		return (0);
 
-	fclose(file);
+	wd = write(STDOUT_FILENO, buffer, rd);
+	if (wd == -1)
+		return (0);
 
-	return ((ssize_t)n);
+	close(file);
+	free(buffer);
+
+	return (wd);
 }
