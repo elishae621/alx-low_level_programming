f28a2b1a67737571d30e8148c3085922e785a33a
Emmanuel Elisha <elishae621@gmail.com>
2023-04-07 08:15:50 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: const string filename
+ * @letters: number of chars to print
+ * Return: actual number of letters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char c;
+	size_t n = 0, printed = 0;
+
+	FILE *file = fopen(filename, "r");
+
+	if (file == NULL)
+		return (0);
+
+	while ((c = fgetc(file)) != EOF && n < letters)
+	{
+		printed =  printf("%c", c);
+		if (printed != 1)
+			return (0);
+		n += printed;
+	}
+
+	return ((ssize_t)n);
+}
